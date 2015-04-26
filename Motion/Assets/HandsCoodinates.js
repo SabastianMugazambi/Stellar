9ac939981dd3f99794f4da67a6d0a727a89a0a8a
Sabastian Mugazambi <mugazambi@gmail.com>
2015-04-25 20:37:08 -0500

    Done vertual reactions

---
+++ Motion/Assets/HandsCoodinates.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+//function Update () {
+//	frame = controller.frame();
+//	hands = frame.hands;
+//	first_hand = hands[0];
+//	print(first_hand);
+//}
---
+++ Motion/Assets/handFinderInterface.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+//
+//function Update () {
+//	if(Time.frameCount%20 == 0) {
+////		var leftPalm = GameObject.Find("palm");
+////		if(leftPalm != null) {
+////			var tran = leftPalm.transform;
+////			var pos = transform.position;
+////			print(pos.x);
+////		}
+//		DebugDeeper(GameObject.Find("LeapOVRPlayerController").transform);
+//	}
+//}
+//
+//
+//function DebugRoot() {
+//	print("-----Debug root -----");
+//	var children = transform.root.GetChildCount();
+//	print("[" + children + "]");
+//	for(var child=0; child < children; child++) {
+//		print("{");
+//		DebugDeeper(transform.GetChild(child));
+//		print("}");
+//	}
+//}
+//
+//function DebugDeeper(trans : Transform) {
+//	var children = trans.GetChildCount();
+//	print(trans.name);
+//	print(children);
+//	print(":");
+//	
+//	
+//	for(var child=0; child<children; child++) {
+//		print("{");
+//		DebugDeeper(trans.GetChild(child));
+//		print("}");
+//	}
+//}
