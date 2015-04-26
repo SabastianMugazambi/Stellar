54c4516738c14b4c379b36f18b4bf6ede273aaaf
KurtGodel <mtredding@yahoo.com>
2015-04-25 22:08:31 -0500

    added some scripts

---
+++ Motion/Assets/CameraMovement.js
@@ -0,0 +1,114 @@
+﻿var smooth : float = 1.5f; // The relative speed at which the camera will catch up.
+var newPos : Vector3;               // The position the camera is trying to reach.
+var speed: float;
+
+var centerOfPlanets = new Array(9);
+for (var i=0; i < centerOfPlanets.length; i++){
+	var temp = [];
+	for (var j=0; j<4; j++){
+		temp[j] = 0;
+	}
+	centerOfPlanets[i] = temp;
+}
+
+	 
+
+function move(direction : Vector3) {
+	var index = -1;
+	var minD = 1e100;
+	var planet = new Array(4);
+	for(var i=0; i<centerOfPlanets.length; i++) {
+		planet = centerOfPlanets[i];
+		var vector: Vector3;
+		vector.x = planet[0];
+		vector.y = planet[1];
+		vector.z = planet[2];
+		var d = dist(vector, gameObject.transform.position);
+		if(d < minD) {
+			minD = d;
+			index = i;
+		}
+	}
+	var k = 1;
+	planet = centerOfPlanets[index];
+	gameObject.transform.position.x += k * planet[3] * direction.x; //why index 3 each time??
+	gameObject.transform.position.y += k * planet[3] * direction.y;
+	gameObject.transform.position.z += k * planet[3] * direction.z;
+}
+
+function warp(direction: Vector3){
+	var minShortDist = 10e100;
+	var index = -1;
+	var planet;
+	for (var i=0; i<centerOfPlanets.length; i++) {
+		planet = centerOfPlanets[i];
+		var vector: Vector3;
+		vector.x = planet[0] - gameObject.transform.position.x;
+		vector.y = planet[1] - gameObject.transform.position.y;
+		vector.z = planet[2] - gameObject.transform.position.z;
+		var theta = angle(vector, direction);
+		if(theta < Mathf.PI/6) {
+			var shortDist = Mathf.Sin(theta)*dist(planet, gameObject.transform.position);
+			if(shortDist < minShortDist) {
+				minShortDist = shortDist;
+				index = i;
+			}
+		}
+	}
+	
+	if(index == -1) {
+		// no planet to auto-aim towards
+		var k = 1;
+		var percentageToWarp = magnitude(direction);
+		gameObject.transform.position.x += k * direction.x;
+		gameObject.transform.position.y += k * direction.y;
+		gameObject.transform.position.z += k * direction.z;
+	}
+	else {
+		var maxWarpVect : Vector3;
+		planet = centerOfPlanets[index];
+		maxWarpVect.x = planet[0] - gameObject.transform.position.x;
+		maxWarpVect.y = planet[1] - gameObject.transform.position.y;
+		maxWarpVect.z = planet[2] - gameObject.transform.position.z;
+		
+		var leng = magnitude(maxWarpVect);
+		var unitVector : Vector3;
+		unitVector.x = maxWarpVect.x / leng;
+		unitVector.y = maxWarpVect.y / leng;
+		unitVector.z = maxWarpVect.z / leng;
+		
+		var p = 2;
+		maxWarpVect.x -= unitVector.x * planet[3] * p;
+		maxWarpVect.y -= unitVector.y * planet[3] * p;
+		maxWarpVect.z -= unitVector.z * planet[3] * p;
+		
+		percentageToWarp = magnitude(direction);
+		gameObject.transform.position.x += maxWarpVect.x * percentageToWarp;
+		gameObject.transform.position.y += maxWarpVect.y * percentageToWarp;
+		gameObject.transform.position.z += maxWarpVect.z * percentageToWarp;
+	}
+}
+
+function dist(a : Vector3, b : Vector3) {
+	return Mathf.Sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)+(a.z-b.z)*(a.z-b.z));
+}
+
+function dotproduct(a: Vector3, b: Vector3) {
+	var n = 0.0;
+	for (var i = 0; i < 3; i++) n += a[i] * b[i];
+	return n;
+ }
+ 
+function magnitude(v: Vector3){
+	var n = 0.0;
+	for (var i = 0; i < 3; i++){
+		n += Mathf.Pow(v[i], 2);
+	}
+	return Mathf.Sqrt(n);
+}
+
+function angle(a: Vector3, b:Vector3){
+	return Mathf.Acos(dotproduct(a, b)/(magnitude(a) * magnitude(b)));
+}
+
+
---
+++ Motion/Assets/StarMaker.js
@@ -0,0 +1,82 @@
+﻿#pragma strict
+import System.IO;
+
+
+var ps : ParticleSystem;
+var particles : ParticleSystem.Particle[];
+
+function Start () {
+	var filePath = "Assets/hygdata_v3.csv";
+	ps = GetComponent.<ParticleSystem>();
+	
+	ps.startSpeed = 0;
+	ps.emissionRate = 0;
+
+	var starPositions = new Vector3[119613];
+	var starColors = new Color32[starPositions.length];
+	var starSizes = new float[starPositions.length];
+	var color = 0;
+	
+	print("A");
+	
+	try {
+        // Create an instance of StreamReader to read from a file.
+        print("B");
+        print(filePath);
+        var sr = new StreamReader(filePath+"");
+        print("C");
+        // Read and display lines from the file until the end of the file is reached.
+        var line = sr.ReadLine();
+        line = sr.ReadLine();
+        var j = 0;
+        while (line != null) {
+        	var lineValues = line.Split(","[0]);
+        	//set positions
+        	starPositions[j] = new Vector3(parseFloat(lineValues[3]), parseFloat(lineValues[4]), parseFloat(lineValues[5]));
+        	
+        	color = parseFloat(lineValues[10]);
+        	if (color == 0) {
+    			starColors[j] = Color32(255, 155, 176, 255);
+			} else if (color == 1) {
+    			starColors[j] = Color32(255, 170, 191, 255);
+			} else if (color == 2) {
+    			starColors[j] = Color32(255, 202, 215, 255);
+			} else if (color == 3) {
+    			starColors[j] = Color32(255, 248, 247, 255);
+			} else if (color == 4) {
+    			starColors[j] = Color32(255, 255, 244, 234);
+			} else if (color == 5) {
+    			starColors[j] = Color32(255, 255, 210, 161);
+			} else {
+    			starColors[j] = Color32(255, 255, 204, 111);
+			}
+			
+			
+        	starSizes[j] = (19.63 - parseFloat(lineValues[1]))/3.63;
+        	
+        	j = j + 1;
+            line = sr.ReadLine();
+        }
+        sr.Close();
+    }
+    catch (e) {
+        // Something went wrong, return -1
+        return -1;
+    }
+    
+	var i = 0;
+	// for(i=0; i<starPositions.length; i++) {
+	for(i=0; i<500; i++) {
+		ps.Emit(starPositions[i], Vector3(0, 0, 0), starSizes[i], Mathf.Infinity, starColors[i]);
+	}
+	
+	print("X");
+	print(starPositions[5]);
+	print(starColors[5]);
+	print(starSizes[5]);
+		
+	
+	particles = new ParticleSystem.Particle[ps.maxParticles];
+}
+
+function Update () {}
